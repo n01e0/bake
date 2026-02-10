4418edd6b453b802fe02f5b2a613c8dd973b5100
n01e0 <reoshiseki@gmail.com>
2026-02-10 11:58:26 +0900

    feat: add DNS query to DoH packet transform

--- src/commands.rs
+++ src/commands.rs
@@ -1,4 +1,17 @@
-use clap::Subcommand;
+use clap::{Subcommand, ValueEnum};
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, ValueEnum)]
+pub enum DnsRecordType {
+    A,
+    Aaaa,
+    Cname,
+    Mx,
+    Ns,
+    Ptr,
+    Soa,
+    Txt,
+    Any,
+}
 
 #[derive(Subcommand)]
 pub enum Commands {
@@ -8,5 +21,15 @@ pub enum Commands {
     },
     UrlDecode,
     FromHex,
+    DnsToDohPacket {
+        #[clap(long = "name")]
+        name: Option<String>,
+        #[clap(long = "type", value_enum, default_value_t = DnsRecordType::A)]
+        qtype: DnsRecordType,
+        #[clap(long = "id", default_value_t = 0)]
+        id: u16,
+        #[clap(long = "endpoint")]
+        endpoint: Option<String>,
+    },
     Defang,
 }
